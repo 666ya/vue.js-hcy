7324827bfa8c268154202eb25fccd8a84419f2c8
焦文儒 <jiaowr@louddt.com>
2022-11-14 10:30:20 +0800

    feat: 第13章异步组件和函数式组件

---
+++ project/pages/13/index.js
@@ -0,0 +1,70 @@
+const MyComponent = {
+    name: 'MyComponent',
+    props: {
+        title: String
+    },
+    data() {
+        return {
+            count: 0
+        }
+    },
+    setup: (props, setupContext) => {
+        const {
+            emit
+        } = setupContext
+        emit('click')
+        return function () {
+            return {
+                type: 'div',
+                children: 'aaa'
+            }
+        }
+    },
+    render() {
+        return {
+            type: 'div',
+            children: [{
+                    type: 'span',
+                    children: `${this.title}：${this.count}`
+                },
+                {
+                    type: 'button',
+                    children: '点击',
+                    props: {
+                        style: 'margin-left: 10px',
+                        onclick: () => {
+                            this.count++
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    mounted() {
+        this.title = 'aaa'
+        console.log(this.count)
+    },
+}
+
+// 无状态组件
+function FunctionalComp(props) {
+    return {
+        type: 'div',
+        children: props.title
+    }
+}
+// 定义props
+FunctionalComp.props = {
+    title: String
+}
+const render = createRenderer(options).render
+const CompVnode = {
+    type: FunctionalComp,
+    props: {
+        title: '组件例子',
+        onClick: () => {
+            console.log('组件事件')
+        }
+    }
+}
+render(CompVnode, document.getElementById('app'))
